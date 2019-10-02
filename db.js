ed22a867f1a0300f8de6343d04d9f1afbc1a3298
asterns <adam.sterns@gmail.com>
2019-10-02 15:11:12 -0400

    try db:test

---
+++ db.js
@@ -0,0 +1,22 @@
+const knex = require('knex');
+
+const config = {
+  user: 'postgres',
+  password: 'secretpassword',
+  database: 'postgres',
+  //port: '5432'
+  //host: 'http://localhost'
+};
+const knexClient = knex({
+  client: 'pg',
+  connection: config,
+  pool: { min: 2, max: 100 },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+});
+knexClient.select()
+  .table("pg_catalog.pg_tables")
+  .then((result) => {
+    console.log(result);
+  });
